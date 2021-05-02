51680b5b394dfbee4db998505453e64cd7044ccd
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-02 21:54:54 +0530

    learning cobra

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,6 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "kumo",
 	Short: "A brief description of your application",
@@ -22,9 +21,10 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
