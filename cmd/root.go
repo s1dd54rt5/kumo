cd4ee38e8bcb589c8d7e75ca2c516e26ea430910
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-05 00:46:55 +0530

    made commands and flags

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,6 @@ type Config struct {
 	KumoPath string
 }
 
-var cfgFile string
 var config Config
 
 var rootCmd = &cobra.Command{
@@ -31,23 +30,15 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "c", "config file (default is $HOME/.kumo.yaml)")
-
 	rootCmd.AddCommand(versionCmd)
 }
 
 func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".kumo")
-		viper.SetConfigType("yaml")
-		fmt.Println(viper.Get("version"))
-	}
+	home, err := homedir.Dir()
+	cobra.CheckErr(err)
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".kumo")
+	viper.SetConfigType("yaml")
 
 	viper.AutomaticEnv()
 	if err := viper.ReadInConfig(); err != nil {
--- cmd/version.go
+++ cmd/version.go
@@ -7,8 +7,11 @@ import (
 )
 
 var (
-	version string = "1.0.0"
+	version     string = "1.0.0"
+	betaVersion string = "1.2.0"
+	beta        bool   = false
 )
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Gives version info for Kumo",
@@ -16,6 +19,16 @@ var versionCmd = &cobra.Command{
 	the files as HTML in your downloads folder`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kumo version " + version)
+		if beta {
+			fmt.Println("kumo version " + betaVersion)
+		} else {
+			fmt.Println("kumo version " + version)
+		}
 	},
+}
+
+func init() {
+	flags := versionCmd.Flags()
+	flags.BoolVar(&beta, "beta", true, "Tells beta version")
+	flags.BoolVar(&beta, "release", false, "Tells release version")
 }
