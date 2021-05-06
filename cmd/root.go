69e593b045ab5ba5eb382d7ff20d0dc9453579eb
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-06 20:26:14 +0530

    done

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	googlesearch "github.com/rocketlaunchr/google-search"
 	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
@@ -21,7 +22,9 @@ var rootCmd = &cobra.Command{
 	Long: `Kumo is a Simple CLI at its core. Just type what you wanna google and get 
 	the files as HTML in your downloads folder`,
 
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(googlesearch.Search(nil, "whats the weather"))
+	},
 }
 
 func Execute() {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/rocketlaunchr/google-search v1.1.2
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 )
