17d56605b72f9ac758e42b356bb2261595f5c147
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2021-05-07 18:42:49 +0530

    done

--- cmd/search.go
+++ cmd/search.go
@@ -2,28 +2,46 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/anaskhan96/soup"
 	googlesearch "github.com/rocketlaunchr/google-search"
 	"github.com/spf13/cobra"
 )
 
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "The command to search the web and the mysteries beyond",
+	Long:  `This command will search the web and put the HTML files in a path of your choice.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		results, err := googlesearch.Search(cmd.Context(), strings.Join(args[:], " "))
+		opts := &googlesearch.SearchOptions{
+			Limit: 1,
+		}
+		results, err := googlesearch.Search(cmd.Context(), strings.Join(args[:], " "), *opts)
 		if err != nil {
 			fmt.Println("Error searching google")
 		}
 		for _, s := range results {
-			fmt.Println(s.URL)
+			resp, err := soup.Get(s.URL)
+			if err != nil {
+				fmt.Println("Cant fetch the site")
+			}
+			s1 := s.URL
+			if last := len(s1) - 1; last >= 0 && s1[last] == '/' {
+				s1 = s1[:last]
+			}
+			split := strings.Split(s1, "/")
+			s1 = split[len(split)-1]
+			file, err := os.Create(config.KumoPath + "/" + s1 + ".html")
+			if err != nil {
+				fmt.Println("Cant create file at path")
+			}
+			_, error := file.WriteString(resp)
+			if error != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Done!")
 		}
 	},
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/44t4nk1/kumo
 go 1.16
 
 require (
+	github.com/anaskhan96/soup v1.2.4 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/rocketlaunchr/google-search v1.1.2
 	github.com/spf13/cobra v1.1.3
